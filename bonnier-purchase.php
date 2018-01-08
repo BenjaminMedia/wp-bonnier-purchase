cff7584d84fadb14c57f479610edde491f2988f8
Magnus Flor Johansen <magnus.johansen@bonnier.dk>
2018-01-08 11:58:20 +0100

    Initial release

--- bonnier-purchase.php
+++ bonnier-purchase.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: Bonnier Purchase Plugin
- * Version: 0.0.1
+ * Version: 1.0.0
  * Plugin URI: https://github.com/BenjaminMedia/wp-bonnier-purchase
  * Description: This plugin allows you to integrate your site with a purchase service
  * Author: Bonnier
